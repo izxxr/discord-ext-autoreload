2accc906c19373d91eb08875a0b50dd25d9396b6
Izhar Ahmad <thisisizharahmad@gmail.com>
2022-07-24 19:04:13 +0500

    Add additional information of package

--- setup.py
+++ setup.py
@@ -7,14 +7,32 @@ with open("requirements.txt", "r") as f:
     install_requires = f.readlines()
 
 
-setup(name='discord-ext-autoreload',
-      author='nerdguyahmad',
-      url='https://github.com/nerdguyahmad/discord-ext-autoreload',
-      version="0.1.1",
-      packages=['discord.ext.autoreload'],
-      long_description=long_description,
-      license='MIT',
-      description='A discord.py extension that allows automatic hot-reloading of extensions',
-      install_requires=install_requires,
-      python_requires='>=3.5.3'
+setup(
+    name='discord-ext-autoreload',
+    author='nerdguyahmad',
+    url='https://github.com/nerdguyahmad/discord-ext-autoreload',
+    keywords=["discord.py", "discord", "reload", "asyncio"],
+    version="0.1.1",
+    packages=['discord.ext.autoreload'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license='MIT',
+    description='A discord.py extension that allows automatic hot-reloading of extensions',
+    install_requires=install_requires,
+    python_requires='>=3.5.3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
 )
