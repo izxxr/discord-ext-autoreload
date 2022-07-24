09cee50d1d479308ed83030408d8b2f94da0215f
Izhar Ahmad <thisisizharahmad@gmail.com>
2022-07-24 18:48:39 +0500

    Add the setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='discord-ext-autoreload',
+      author='nerdguyahmad',
+      url='https://github.com/nerdguyahmad/discord-ext-autoreload',
+      version="0.1.0",
+      packages=['discord.ext.menus'],
+      license='MIT',
+      description='A discord.py extension that allows automatic hot-reloading of extensions',
+      install_requires=['discord.py>=1.7'],
+      python_requires='>=3.5.3'
+)
